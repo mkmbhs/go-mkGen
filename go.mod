994919f9dfbe7f33f98416efbfd2da12a093e4f7
Mkmbhs Kamal <mkmbhs@gmail.com>
2024-03-05 13:51:35 +0300

    Refactor project structure and add project name and service count input validation

---
+++ go.mod
@@ -0,0 +1,18 @@
+module mkgen
+
+go 1.21.5
+
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/fatih/color v1.16.0
+	github.com/schollz/progressbar/v3 v3.14.2
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+)
--- mkGen.go
+++ mkGen.go
@@ -3,42 +3,62 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
+
+	"github.com/common-nighthawk/go-figure"
+	"github.com/fatih/color"
 )
 
 func main() {
+	myFigure := figure.NewFigure("MK Gen", "doom", true)
+	// myFigure.Blink(3000, 500, -1)
+	myFigure.Print()
+	color.Cyan("MK's Mircoservices Project Generator\n")
+
 	reader := bufio.NewReader(os.Stdin)
 
 	// Ask for the project name
 	fmt.Print("Enter the name of the project: ")
-	projectName, _ := reader.ReadString('\n')
+	projectName, err := reader.ReadString('\n')
+	if err != nil {
+		color.Red("Error reading project name: %v", err)
+		return
+	}
 	projectName = strings.TrimSpace(projectName)
 
 	// Ask for the number of services
 	fmt.Print("How many services does your project have? ")
 	var serviceCount int
-	_, err := fmt.Scan(&serviceCount)
+	_, err = fmt.Scan(&serviceCount)
 	if err != nil {
-		log.Fatalf("Error reading service count: %v", err)
+		color.Red("Error reading service count: %v", err)
+		return
 	}
 
 	// Ask for the names of the services
 	serviceNames := make([]string, serviceCount)
 	for i := 0; i < serviceCount; i++ {
 		fmt.Printf("Enter the name of service #%d: ", i+1)
-		serviceName, _ := reader.ReadString('\n')
+		serviceName, err := reader.ReadString('\n')
+		if err != nil {
+			color.Red("Error reading service name: %v", err)
+			return
+		}
 		serviceNames[i] = strings.TrimSpace(serviceName)
 	}
 
 	// Generate the project structure
-	generateProjectStructure(projectName, serviceNames)
+	if err := generateProjectStructure(projectName, serviceNames); err != nil {
+		color.Red("Error generating project structure: %v", err)
+		return
+	}
+
+	color.Green("Project %s created successfully.", projectName)
 }
 
-func generateProjectStructure(projectName string, serviceNames []string) {
+func generateProjectStructure(projectName string, serviceNames []string) error {
 	basePath := filepath.Join(".", projectName)
 
 	// Common directories and files
@@ -60,65 +80,45 @@ func generateProjectStructure(projectName string, serviceNames []string) {
 
 	// Create common structure
 	for _, path := range commonStructure {
-		createPath(basePath, path)
+		if err := createPath(basePath, path); err != nil {
+			return err
+		}
 	}
 
 	// Create service-specific structure
 	for _, serviceName := range serviceNames {
 		serviceStructure := []string{
-			fmt.Sprintf("cmd/%s/main.go", serviceName),
-			fmt.Sprintf("cmd/%s/Dockerfile", serviceName),
-			fmt.Sprintf("deployments/docker/%s/Dockerfile", serviceName),
-			fmt.Sprintf("protobuf/%s.proto", serviceName),
+			fmt.Sprintf("cmd/%s/main.go", "service-"+serviceName),
+			fmt.Sprintf("cmd/%s/Dockerfile", "service-"+serviceName),
+			fmt.Sprintf("deployments/docker/%s/Dockerfile", "service-"+serviceName),
+			fmt.Sprintf("protobuf/%s.proto", "service-"+serviceName),
 		}
 
 		for _, path := range serviceStructure {
-			createPath(basePath, path)
+			if err := createPath(basePath, path); err != nil {
+				return err
+			}
 		}
-
-		// Initialize Go module for each service
-		initGoModule(filepath.Join(basePath, "cmd", serviceName), serviceName)
 	}
+
+	return nil
 }
 
-func createPath(basePath, path string) {
+func createPath(basePath, path string) error {
 	fullPath := filepath.Join(basePath, path)
 	if filepath.Ext(path) == "" { // If no file extension, assume directory
-		log.Printf("Creating directory: %s\n", fullPath)
-		if err := os.MkdirAll(fullPath, os.ModePerm); err != nil {
-			log.Fatalf("Failed to create directory %s: %v", fullPath, err)
+		if err := os.MkdirAll(fullPath, 0755); err != nil {
+			return fmt.Errorf("failed to create directory %s: %v", fullPath, err)
 		}
 	} else { // Otherwise, create file
-		log.Printf("Creating file: %s\n", fullPath)
-		if err := os.MkdirAll(filepath.Dir(fullPath), os.ModePerm); err != nil {
-			log.Fatalf("Failed to create directory for file %s: %v", fullPath, err)
+		if err := os.MkdirAll(filepath.Dir(fullPath), 0755); err != nil {
+			return fmt.Errorf("failed to create directory for file %s: %v", fullPath, err)
 		}
 		file, err := os.Create(fullPath)
 		if err != nil {
-			log.Fatalf("Failed to create file %s: %v", fullPath, err)
+			return fmt.Errorf("failed to create file %s: %v", fullPath, err)
 		}
-		file.Close()
+		defer file.Close()
 	}
-}
-
-func initGoModule(servicePath, serviceName string) {
-	log.Printf("Initializing Go module for service %s\n", serviceName)
-
-	// Change to the service directory
-	os.Chdir(servicePath)
-
-	// Run `go mod init`
-	cmd := exec.Command("go", "mod", "init", serviceName)
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Failed to initialize Go module for service %s: %v", serviceName, err)
-	}
-
-	// Run `go mod tidy`
-	cmd = exec.Command("go", "mod", "tidy")
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Failed to tidy Go module for service %s: %v", serviceName, err)
-	}
-
-	// Change back to the original directory
-	os.Chdir("../../..")
+	return nil
 }
